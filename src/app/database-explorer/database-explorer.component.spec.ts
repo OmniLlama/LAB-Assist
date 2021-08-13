85268894826d6b5beaaf1704cbd1479436d018cc
Jeffrey Hanson <omnillama@gmail.com>
2021-08-12 20:16:00 -0500

    post NG11 upgrade

--- src/app/database-explorer/database-explorer.component.spec.ts
+++ src/app/database-explorer/database-explorer.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DatabaseExplorerComponent } from './database-explorer.component';
 
@@ -6,7 +6,7 @@ describe('DatabaseExplorerComponent', () => {
   let component: DatabaseExplorerComponent;
   let fixture: ComponentFixture<DatabaseExplorerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DatabaseExplorerComponent ]
     })
--- src/app/input-converter/input-converter.component.spec.ts
+++ src/app/input-converter/input-converter.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InputConverterComponent } from './input-converter.component';
 
@@ -6,7 +6,7 @@ describe('InputConverterComponent', () => {
   let component: InputConverterComponent;
   let fixture: ComponentFixture<InputConverterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InputConverterComponent ]
     })
